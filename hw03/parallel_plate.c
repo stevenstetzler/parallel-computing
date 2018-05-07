e61141d65f9c9bce293fd2c10b9d2eebb2da2ec5
stevenstetzler <steven.stetzler@gmail.com>
2018-05-07 07:13:37 -0400

    Udate parallel plate code

--- hw03/parallel_plate.c
+++ hw03/parallel_plate.c
@@ -1,9 +1,8 @@
-// This program simulates the flow of heat through a two-dimensional plate.
-// The number of grid cells used to model the plate as well as the number of
-//  iterations to simulate can be specified on the command-line as follows:
-//  ./heated_plate_sequential <columns> <rows> <iterations>
-// For example, to execute with a 500 x 500 grid for 250 iterations, use:
-//  ./heated_plate_sequential 500 500 250
+/* 
+ * CS 4444
+ * Steven Stetzler
+ * Homework 3: Heated Plate With Pthreads
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,7 +18,7 @@
 #define INITIAL_CELL_VALUE 50.0
 #define hotSpotRow 4500
 #define hotSpotCol 6500
-#define hotSpotTemp 1000
+#define hotSpotTemp 100.0
 
 // Function prototypes
 void print_cells(float **cells, int n_x, int n_y);
@@ -28,6 +27,11 @@ void create_snapshot(float **cells, int n_x, int n_y, int id);
 float **allocate_cells(int n_x, int n_y);
 void die(const char *error);
 
+// struct to store arguments that each thread should carry with it
+// Each thread should know which rows and columns of the plate it should work on
+// given by start_row, end_row, start_col, and end_col
+// Each thread should also carry with it the location of the plate in memory
+// and a barrier to know when to stop and wait for othe threads to catch up to it
 typedef struct args {
     int start_row;
     int start_col;
@@ -41,20 +45,29 @@ typedef struct args {
     pthread_barrier_t* update_barrier;
 } args;
 
+// Global variables that are used throughout
+// make_movie tells us whether or not to print out a snapshot every few iterations
 int make_movie = 0;
+// curr_iter tells us which iteration should be snapshotted next
+// curr_iter should only be modified by a single thread at a time
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 int curr_iter = 0;
 
-void* parallel_code(void* p) { 
+// This is the function containing the work that each thread will perform
+void* thread_work(void* p) { 
+    // Get this threads parameters
     args* t_args = (struct args*) p;
     
+    // Get the plate to work with
     float*** cells = t_args->plate;
 
     int t, y, x;
     int next_cells_index = 1;
     int curr_cells_index = 0;
 
+    // Print this thread's parameters (as a sanity check)
     printf("Thread %d, start_row = %d, end_row = %d, start_col = %d, end_col = %d\n", t_args->thread_id, t_args->start_row, t_args->end_row, t_args->start_col, t_args->end_col);
+
     // Simulate the heat flow for the specified number of iterations
 	for (t = 0; t < t_args->iterations; t++) {
 		// Traverse the plate, computing the new value of each cell
@@ -69,41 +82,40 @@ void* parallel_code(void* p) {
 				// printf("Thread %d updated (%d, %d) to %f\n", t_args->thread_id, y, x, cells[next_cells_index][y][x]);
             }
 		}
-		
+        
+        // All threads should wait for the others to finish updating the t + 1 plate        
         pthread_barrier_wait(t_args->update_barrier);
-		// printf("Thread %d starting up again.\n", t_args->thread_id);
 
         // Swap the two arrays
 		curr_cells_index = next_cells_index;
 		next_cells_index = !curr_cells_index;
 
+        // Update the hot spot temperature
         if (hotSpotRow <= t_args->num_rows && hotSpotCol <= t_args->num_cols) {
 		    cells[curr_cells_index][hotSpotRow][hotSpotCol] = hotSpotTemp;
         }
 
+        // Check if we're supposed to make a movie
         if (make_movie == 1) {
-            // First thread that gets to this creates a snapshot at current iter, increments iter
+            // First thread that gets to this creates a snapshot at current iter and then increments iter
             pthread_mutex_lock(&mutex1);
 
+            // Only snapshot on certain frames (we will make at most 100 snapshots of the system)
             if (curr_iter == t) {
-                printf("Thread %d, curr_iter = %d, t = %d\n", t_args->thread_id, curr_iter, t);
                 create_snapshot(cells[curr_cells_index], t_args->num_cols, t_args->num_rows, curr_iter);
                 curr_iter += (t_args->iterations > 100) ? t_args->iterations / 100 : 1;
-                printf("Thread %d, Update: curr_iter = %d, t = %d\n", t_args->thread_id, curr_iter, t);
             }
 
             pthread_mutex_unlock(&mutex1);
         }
 
-        pthread_barrier_wait(t_args->update_barrier);
-		// printf("Thread %d starting up again.\n", t_args->thread_id);
-        
-        // Print the current progress
-		printf("Thread: %d, Iteration: %d / %d\n", t_args->thread_id, t + 1, t_args->iterations);
+        // All threads should wait to make sure everyone switched their arrays and that the movie finished processing
+        pthread_barrier_wait(t_args->update_barrier);    
 	}
     
 }
 
+// This helper function checks if a number is a power of 4
 int power_of_four(unsigned int x) {
     if (x == 0) 
         return 0;
@@ -112,6 +124,7 @@ int power_of_four(unsigned int x) {
     return x & 0x55555555;
 }
 
+// Main function
 int main(int argc, char **argv) {
 	// Record the start time of the program
 	time_t start_time = time(NULL);
@@ -125,6 +138,7 @@ int main(int argc, char **argv) {
 	int iterations = (argc > 3) ? atoi(argv[3]) : 100;
     // Number of threads to use
     unsigned int num_threads = (argc > 4) ? atoi(argv[4]) : 1;
+    // Specify the method of work distribution for the threads. See below for specific details
     int process_layout = (argc > 5) ? atoi(argv[5]) : 0;
     // Whether or not to make a movie
     make_movie = (argc > 6) ? atoi(argv[6]) : 0;
@@ -155,26 +169,26 @@ int main(int argc, char **argv) {
 	for (y = 1; y <= num_rows; y++) cells[0][y][num_cols + 1] = cells[1][y][num_cols + 1] = RIGHT_BOUNDARY_VALUE;
 
     // Create a set of threads
-
     pthread_t threadpool[num_threads];
     args arguments[num_threads];
 
+    // Initialize thread attributes
     pthread_attr_t attr;
     pthread_attr_init(&attr);
 
+    // Make a barrier that waits for all of the threads
     pthread_barrier_t update_barrier;
     pthread_barrier_init(&update_barrier, NULL, num_threads);
-    
+   
+    // The number of squares we can divide the plate into 
     int divisions = (int) sqrt(num_threads);
-    if (process_layout == 3) {
-        divisions = num_rows / 100;
-    }
+    // Counter to help specify row, column assignment
     int j = 0;
-
-    int necessary_subproblems = (2 * num_rows * num_cols * 32) / (2048 * 1024);
 
     // Define scheme for assigning which work each thread will take
     for (i = 0; i < num_threads; i++) {
+        // Check which distribution method we are using
+        // Assign rows and columns that each thread should work on accordingly
         switch (process_layout) {
             case 0 :
                 // Assign each thread an equal number of rows to work with
@@ -191,7 +205,8 @@ int main(int argc, char **argv) {
                 arguments[i].end_col = (int) (num_cols / (float) num_threads * (i + 1));
                 break;
             case 2 :
-                // Block into squares
+                // Block problem into squares of equal size
+                // We need to be using a power of 4 threads for this to work
                 if (!power_of_four(num_threads)) {
                     printf("num_threads = %d is not a power of four. Cannot block into squares.\n", num_threads);
                     return 1;
@@ -203,23 +218,11 @@ int main(int argc, char **argv) {
                 arguments[i].end_row = (int) (num_rows / (float) divisions * ((i % divisions) + 1));
                 arguments[i].start_col = (int) (num_cols / (float) divisions * j) + 1;
                 arguments[i].end_col = (int) (num_cols / (float) divisions * (j + 1));
-                //printf("Thread %d, start_row %d, end_row %d, start_col %d, end_col %d\n", i, arguments[i].start_row, arguments[i].end_row, arguments[i].start_col, arguments[i].end_col);
-                
-                break;
-            case 3 :
-                // Block into cache sized squares
-                if (i % divisions == 0 && i != 0) {
-                    j += 1;
-                }
-                arguments[i].start_row = (int) (num_rows / (float) divisions * (i % divisions)) + 1;
-                arguments[i].end_row = (int) (num_rows / (float) divisions * ((i % divisions) + 1));
-                arguments[i].start_col = (int) (num_cols / (float) divisions * j) + 1;
-                arguments[i].end_col = (int) (num_cols / (float) divisions * (j + 1));
-                //printf("Thread %d, start_row %d, end_row %d, start_col %d, end_col %d\n", i, arguments[i].start_row, arguments[i].end_row, arguments[i].start_col, arguments[i].end_col);
                 
                 break;
         }
 
+        // Assign other parameter values
         arguments[i].thread_id = i;
         arguments[i].iterations = iterations;
         arguments[i].plate = cells;
@@ -228,21 +231,14 @@ int main(int argc, char **argv) {
         arguments[i].num_cols = num_cols;
     }
 
-    //return 0;
-
     for (i = 0; i < num_threads; i++) {
-        printf("Starting thread %d.\n", i);
         // Create each thread, passing thread attributes, the function to run, and the arguments to use
-        pthread_create(&threadpool[i], &attr, &parallel_code, (void *) &arguments[i]);
+        pthread_create(&threadpool[i], &attr, &thread_work, (void *) &arguments[i]);
     }
 
     for (i = 0; i < num_threads; i++) {
         pthread_join(threadpool[i], NULL);
     }
-
-	// Output a snapshot of the final state of the plate
-	int final_cells = (iterations % 2 == 0) ? 0 : 1;
-	//create_snapshot(cells[final_cells], num_cols, num_rows, iterations);
 
 	// Compute and output the execution time
 	time_t end_time = time(NULL);
@@ -254,12 +250,15 @@ int main(int argc, char **argv) {
 
 // Allocates and returns a pointer to a 2D array of floats
 float **allocate_cells(int num_cols, int num_rows) {
-	float **array = (float **) malloc(num_rows * sizeof(float *));
+	// Initialize array of float pointers -- array of arrays
+    float **array = (float **) malloc(num_rows * sizeof(float *));
 	if (array == NULL) die("Error allocating array!\n");
 	
+    // Initialize first pointer to point to beginning of flattened array of size num_rows x num_cols
 	array[0] = (float *) malloc(num_rows * num_cols * sizeof(float));
 	if (array[0] == NULL) die("Error allocating array!\n");
 
+    // Initialize each row to point to a portion of the flattened array that is of size num_cols
 	int i;
 	for (i = 1; i < num_rows; i++) {
 		array[i] = array[0] + (i * num_cols);
@@ -309,7 +308,7 @@ void create_snapshot(float **cells, int num_cols, int num_rows, int id) {
 	
 	// Open/create the file
 	char text[255];
-	sprintf(text, "snapshot_parallel.%04d.ppm", id);
+	sprintf(text, "snapshot_parallel.%06d.ppm", id);
 	FILE *out = fopen(text, "w");
 	// Make sure the file was created
 	if (out == NULL) {
@@ -336,8 +335,10 @@ void create_snapshot(float **cells, int num_cols, int num_rows, int id) {
 			}
 			// Write out the average value of the cells we just visited
 			int average = (int) (sum * inverse_cells_per_pixel);
+			// printf("%d 0 %d\t", average, 100 - average);
 			fprintf(out, "%d 0 %d\t", average, 100 - average);
 		}
+        // printf("\n");
 		fwrite("\n", sizeof(char), 1, out);
 	}
 	
